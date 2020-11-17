4af813a031cdb06a3042bccca75d8d44171a3f94
PedroAntunes178 <pedronmantunes@gmail.com>
2020-11-17 16:49:52 +0000

    finished example test

--- circuit.v
+++ circuit.v
@@ -1,17 +1,25 @@
 `timescale 1ns / 1ps
 `include "iob_lib.vh"
 
+`define W = 32
 module circuit
+  #(
+    parameter W = 32
+    )
   (
    `INPUT(rst,1),
    `INPUT(clk,1),
    `INPUT(en,1),
-   `INPUT(x,32),
-   `OUTPUT(y,32)
+   `OUTPUT(f,W)
    );
+   `SIGNAL(cnt, 7)
+   `SIGNAL(f_final, W)
+   `SIGNAL(f_final_1, W)
+   `SIGNAL(f_final_2, W)
+   `SIGNAL2OUT(f, f_final)
 
-   `SIGNAL(acc, 32)
-   `ACC_ARE(clk, rst, 1'b0, en, acc, x)
-   `SIGNAL2OUT(y, acc)
-   
+   `REG_RE(clk, rst, 1'b0, en & (cnt!=99), f_final_1, f_final)
+   `REG_RE(clk, rst, 1'b0, en & (cnt!=99), f_final_2, f_final_1)
+   `COUNTER_RE(clk, rst, en & (cnt!=99), cnt)
+   `COMB f_final = f_final_1 + f_final_2 + (cnt == 1'b1);
 endmodule
